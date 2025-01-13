fb36ad03b9de6c5cc1a13b78e2b07ffca4f26a93
sunggyu <jskoder0109@gmail.com>
2025-01-13 17:09:29 +0900

    feat(user): slient refresh 개발

--- controller/userController.js
+++ controller/userController.js
@@ -1,22 +1,39 @@
-const { hashedPassword } = require("../comm/utils");
-const { tbl_users } = require("../public/database/models");
-const bcrypt = require("bcrypt");
+import {
+  decodedToken,
+  generateToken,
+  verifyRefreshToken,
+} from "../comm/jwt.js";
+import { compareSync } from "bcrypt";
+import tbl_users from "../public/database/models/tbl_users.js";
 
-exports.getUser = async (req, res) => {
+export async function getUser(req, res) {
   try {
     // ID로 유저 정보 조회
     const user = await tbl_users.findOne({
       where: { user_id: req.query.user_id },
     });
-    // 비밀번호와 데이터베이스 정보가 일치하는 지 확인
-    if (bcrypt.compareSync(req.query.password, user.password)) {
+    // 사용자가 전달한 비밀번호와 데이터베이스 비밀번호 정보가 일치하는 지 확인
+    if (compareSync(req.query.password, user.password)) {
+      const tokensInfomation = {
+        user_id: user.user_id,
+        created_at: user.created_at,
+      };
+      const tokens = generateToken(tokensInfomation);
+      res.cookie("refreshToken", tokens.refreshToken, {
+        expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7일
+        httpOnly: true,
+        path: "/",
+      });
+      const accessToken = tokens.accessToken;
       res.status(200).json({
         status: 200,
         message: "success",
         messageDev: "유저 정보 조회 성공",
-        data: user,
+        data: {
+          accessToken: accessToken,
+        },
       });
-    } else if (!bcrypt.compareSync(req.query.password, user.password)) {
+    } else if (!compareSync(req.query.password, user.password)) {
       // 비밀번호가 일치하지 않는 경우 401 Unauthorized
       res.status(401).json({
         status: 401,
@@ -40,9 +57,9 @@ exports.getUser = async (req, res) => {
       messageDev: "유저 정보 조회 실패",
     });
   }
-};
+}
 
-exports.postUser = async (req, res) => {
+export async function postUser(req, res) {
   try {
     const user = await tbl_users.create(req.body);
     res.status(201).json({
@@ -59,9 +76,9 @@ exports.postUser = async (req, res) => {
       messageDev: "유저 등록 실패",
     });
   }
-};
+}
 
-exports.putUser = async (req, res) => {
+export async function putUser(req, res) {
   try {
     const user = await tbl_users.findOne({
       where: { user_id: req.body.user_id },
@@ -94,9 +111,9 @@ exports.putUser = async (req, res) => {
       messageDev: "유저 정보 수정 실패",
     });
   }
-};
+}
 
-exports.deleteUser = async (req, res) => {
+export async function deleteUser(req, res) {
   try {
     const user = await tbl_users.findOne({
       where: { user_id: req.query.user_id },
@@ -124,4 +141,43 @@ exports.deleteUser = async (req, res) => {
       messageDev: "유저 정보 삭제 실패",
     });
   }
-};
+}
+
+export async function silentRefresh(req, res) {
+  try {
+    const refreshToken = req.cookies.refreshToken;
+    // 리프래시 토큰이 유효한지 검증 및 복호화
+    const decodedValue = verifyRefreshToken(refreshToken);
+
+    const tokensInfomation = {
+      user_id: decodedValue.user_id,
+      created_at: decodedValue.created_at,
+    };
+    // 토큰 정보로 새로운 access token, refresh token 발행
+    const tokens = generateToken(tokensInfomation);
+
+    // RTR: 리프래시 토큰 갱신
+    res.cookie("refreshToken", tokens.refreshToken, {
+      expires: new Date(Date.now() + 7 * 24 * 60 * 60 * 1000), // 7일
+      httpOnly: true,
+      path: "/",
+    });
+    // 엑세스 토큰 반환
+    res.status(200).json({
+      status: 200,
+      message: "success",
+      messageDev: "OAuth Refresh Token 발행 성공(RTR)",
+      data: {
+        accessToken: tokens.accessToken,
+      },
+    });
+  } catch (error) {
+    console.log(">>>>>>>> OAuth Refresh Token 발행 중 오류 발생", error);
+    res.cookie("refreshToken", "", { expires: new Date(0), httpOnly: true });
+    res.status(409).json({
+      status: 409,
+      message: "OAuth Refresh Token 발행 중 오류가 발생했습니다.",
+      messageDev: "OAuth Refresh Token 발행 실패",
+    });
+  }
+}
--- routes/user.js
+++ routes/user.js
@@ -1,15 +1,17 @@
-const express = require("express");
-const {
+import { Router } from "express";
+import {
   getUser,
   postUser,
   putUser,
   deleteUser,
-} = require("../controller/userController");
-const router = express.Router();
+  silentRefresh,
+} from "../controller/userController.js";
+const router = Router();
 
 router.get("/user", getUser);
+router.post("/user/refresh", silentRefresh);
 router.post("/user", postUser);
 router.put("/user", putUser);
 router.delete("/user", deleteUser);
 
-module.exports = router;
+export default router;
