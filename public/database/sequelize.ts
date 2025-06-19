2816c3aa9d6f2cc344adb819f3d3a608d5e3ee40
sunggyu <jskoder0109@gmail.com>
2025-06-19 14:21:07 +0900

    feat. ssl 제거

--- public/database/sequelize.ts
+++ public/database/sequelize.ts
@@ -10,12 +10,6 @@ if (!process.env.DATABASE_URL) {
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
-  dialectOptions: {
-    ssl: {
-      require: true,
-      rejectUnauthorized: false,
-    },
-  },
 });
 
 export default sequelize;
