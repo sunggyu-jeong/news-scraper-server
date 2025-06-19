b61260fc807de6b0f1074110f247302f2a361f87
sunggyu <jskoder0109@gmail.com>
2025-06-19 14:20:04 +0900

    feat. ssl 설정 롤백

--- public/database/sequelize.ts
+++ public/database/sequelize.ts
@@ -10,6 +10,12 @@ if (!process.env.DATABASE_URL) {
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   protocol: 'postgres',
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
 
 export default sequelize;
